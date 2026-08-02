6f9df3cffd8df8e28dcd7698a79267c29c3e7773
ln9swrd <129256046+ln9swrd@users.noreply.github.com>
2026-08-02 15:40:01 +0900

    chore(paramodel): bump addon to v0.2.0

--- projects/paramodel/addon/__init__.py
+++ projects/paramodel/addon/__init__.py
@@ -1,10 +1,10 @@
 bl_info = {
     "name": "ParaModel",
     "author": "Atlas / Excelion",
-    "version": (0, 1, 0),
+    "version": (0, 2, 0),
     "blender": (3, 6, 0),
     "location": "View3D > Sidebar > ParaModel",
-    "description": "Parametric mecha model loader - Base Body slots + metadata",
+    "description": "Parametric mecha model loader - Base Body slots + placeholders",
     "category": "Object",
 }
 
@@ -18,7 +18,7 @@ def register():
     props.register()
     operators.register()
     panels.register()
-    print("ParaModel addon registered (v0.1.0)")
+    print("ParaModel addon registered (v0.2.0)")
 
 
 def unregister():
